4e916cc4cf6fa6031c86ee4517983ceb0b188e5c
unknown <mekh.aga@mail.ru>
2021-05-11 23:24:42 +0300

    update

--- src/Pages/dashboard/index.js
+++ src/Pages/dashboard/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "../../Components/Header";
+import Helmet from "react-helmet";
 class Dashboard extends React.Component {
   render() {
     return (
       <>
+        <Helmet title="Панель управления" />
         <Header />
       </>
     );
